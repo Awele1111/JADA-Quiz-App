5e659c2cab97062a773ab08188673b395ac92955
allisonnault <allisonrnault@gmail.com>
2023-04-04 11:54:37 -0500

    typeDefs and resolvers in progress

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,35 @@
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Quiz } = require('../models')
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, { _id }) => {
+      return User.findOne({ _id });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("you need to be logged in");
+    },
+    quizzes: async () => {
+      return Quiz.find();
+    },
+    quiz: async (parent, { _id }) => {
+      return Quiz.findOne({ _id });
+    }
   },
 
   Mutation: {
-   
+   addUser: async (parent, { username, email, password }) => {
+    const user = await User.create({ username, email, password });
+    const token = signToken(user);
+    return { token, user };
+   }
   }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,56 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        favoriteQuizes: [Quiz]
+    }
+
+    type Choice {
+        choice: String
+        correct: Boolean
+    }
+
+    type Question {
+        question: String
+        choices: [Choice]
+    }
+
+    type Attempt {
+        userId: User
+        score: Int
+    }
+
+    type Quiz {
+        _id: ID
+        title: String
+        creator: User
+        public: Boolean
+        style: String
+        questions: [Question]
+        highscores: [Attempt]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
   
     type Query {
-        
+        quizzes: [Quiz]
+        users: [User]
+        user(_id: ID!): User
+        quiz(_id: ID!): Quiz
+        me: User
     }
 
     type Mutation {
-        
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        createQuiz(title: String!, creator: String, public: Boolean, style: String, questions: [String]!): User
     }
 `;
 
