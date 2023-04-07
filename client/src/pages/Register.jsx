7b310f959085e68887dc659c69115c717363fdb3
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-04-07 13:36:01 -0500

    added messages to login & signup pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,98 +1,3 @@
-// import React, {useState} from "react";
-// import "../css/register.css";
-// import { useMutation } from "@apollo/client";
-// import { ADD_USER } from "../utils/mutations";
-// import Auth from '../utils/auth';
-
-// export const Register = (props) => {
-//     const [addUser, { error, data }] = useMutation(ADD_USER);
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [username, setUsername] = useState('');
-
-//     const  handleSubmit = async (e) =>  {
-//         e.preventDefault();
-//         console.log(email);
-
-//         try {
-//             const { data } = await addUser({
-//                 variables: { email, password, username }
-//             });
-            
-//             Auth.login(data.addUser.token);
-
-//         } catch (e) {
-//             console.error(e);
-//         }
-//     }
-//     return (
-//         <div className="register-body">
-//         <body className="register-body">
-//         <div className="auth-form-container">
-//         <h2 className="register-h2">Register</h2>
-//         <form className="register-form"onSubmit={handleSubmit}>
-//             <label className="register-label" htmlFor="name">Username</label>
-//             <input className="register-input"   name="name" value={username} onChange={(e) => setUsername(e.target.value)} id="name" placeholder="Type your username here"/>
-//             <label className="register-label" value="name" htmlFor="email">Email Address</label>
-//             <input className="register-input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Type your email here" id="email" name="email" />
-//             <label className="register-label" htmlFor="password">Password</label>
-//             <input className="register-input" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-//             <button className="register-button" type="submit">Register/signup</button>
-//         </form>
-        
-//         <button className="link-button" onClick={() => window.location.assign('/login')}>Already have an account? Login here</button>
-//         </div>
-//     </body>
-//     </div>
-//     )
-// }
-
-// const Register = (props) => {
-//     const [addUser, { error, data }] = useMutation(ADD_USER);
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [username, setUsername] = useState('');
-//     const [errorMessage, setErrorMessage] = useState('');
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//             const { data } = await addUser({
-//                 variables: { email, password, username }
-//             });
-//             Auth.login(data.addUser.token);
-//         } catch (e) {
-//             console.error(e);
-//             if (e.graphQLErrors.length > 0) {
-//                 const errorMessage = e.graphQLErrors[0].message;
-//                 setErrorMessage(errorMessage);
-//             } else {
-//                 setErrorMessage("An error occurred during registration. Please try again.");
-//             }
-//         }
-//     }
-
-//     return (
-//         <div className="register-body">
-//             <body className="register-body">
-//                 <div className="auth-form-container">
-//                     <h2 className="register-h2">Register</h2>
-//                     <form className="register-form" onSubmit={handleSubmit}>
-//                         {errorMessage && <p className="error-message">{errorMessage}</p>}
-//                         <label className="register-label" htmlFor="name">Username</label>
-//                         <input className="register-input" name="name" value={username} onChange={(e) => setUsername(e.target.value)} id="name" placeholder="Type your username here" />
-//                         <label className="register-label" value="name" htmlFor="email">Email Address</label>
-//                         <input className="register-input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Type your email here" id="email" name="email" />
-//                         <label className="register-label" htmlFor="password">Password</label>
-//                         <input className="register-input" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-//                         <button className="register-button" type="submit">Register/signup</button>
-//                     </form>
-//                     <button className="link-button" onClick={() => window.location.assign('/login')}>Already have an account? Login here</button>
-//                 </div>
-//             </body>
-//         </div>
-//     )
-// }
 
 import React, { useState } from "react";
 import "../css/register.css";
@@ -101,54 +6,52 @@ import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 export const Register = (props) => {
-  const [addUser, { error }] = useMutation(ADD_USER);
+  const [addUser, { error, data }] = useMutation(ADD_USER);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(email);
 
-    try {
-      const { data } = await addUser({
-        variables: { email, password, username },
-      });
+    if (email && username && password) {
+      try {
+        const { data } = await addUser({
+          variables: { email, password, username },
+        });
 
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-      setErrorMessage(e.message);
+        Auth.login(data.addUser.token);
+      } catch (e) {
+        document.getElementById("registerUsernameError").innerHTML = "";
+        document.getElementById("registerEmailError").innerHTML = "";
+        console.log(e.message);
+        if (e.message.includes("E11000") && e.message.includes("username")) {
+          console.log("This Username is already taken!");
+          document.getElementById("registerUsernameError").innerHTML =
+            "This Username is already taken!";
+        } else if (e.message.includes("E11000") && e.message.includes("email")) {
+          console.log("This Email is already taken!");
+          document.getElementById("registerEmailError").innerHTML =
+            "This Email is already taken!";
+        } else if (e.message.includes("email")) {
+          console.log("Your email is not a proper email address!");
+          document.getElementById("registerEmailError").innerHTML =
+            "Your email is not a proper email address!";
+        } else {
+          console.log("An error occurred. Please try again.");
+        }
+      }
+    } else {
+      console.log("All users require a username, password, and email");
     }
   };
-
-  let emailError = "";
-  let passwordError = "";
-  let usernameError = "";
-
-  if (error) {
-    const errorMessages = error.graphQLErrors.map((err) => err.message);
-    errorMessages.forEach((msg) => {
-      if (msg.includes("email")) {
-        emailError = msg;
-      } else if (msg.includes("password")) {
-        passwordError = msg;
-      } else if (msg.includes("username")) {
-        usernameError = msg;
-      } else {
-        setErrorMessage(msg);
-      }
-    });
-  }
 
   return (
     <div className="register-body">
       <body className="register-body">
         <div className="auth-form-container">
           <h2 className="register-h2">Register</h2>
-          {errorMessage && (
-            <p className="error-message">{errorMessage}</p>
-          )}
           <form className="register-form" onSubmit={handleSubmit}>
             <label className="register-label" htmlFor="name">
               Username
@@ -161,9 +64,7 @@ export const Register = (props) => {
               id="name"
               placeholder="Type your username here"
             />
-            {usernameError && (
-              <p className="error-message">{usernameError}</p>
-            )}
+            <div id="registerUsernameError"></div>
             <label className="register-label" value="name" htmlFor="email">
               Email Address
             </label>
@@ -176,9 +77,7 @@ export const Register = (props) => {
               id="email"
               name="email"
             />
-            {emailError && (
-              <p className="error-message">{emailError}</p>
-            )}
+            <div id="registerEmailError"></div>
             <label className="register-label" htmlFor="password">
               Password
             </label>
@@ -191,9 +90,6 @@ export const Register = (props) => {
               id="password"
               name="password"
             />
-            {passwordError && (
-              <p className="error-message">{passwordError}</p>
-            )}
             <button className="register-button" type="submit">
               Register/signup
             </button>
