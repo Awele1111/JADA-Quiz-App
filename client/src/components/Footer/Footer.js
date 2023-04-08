957185edb9446edf8b56819cc0d6489d5376bd6c
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-04-08 17:05:10 -0500

    adding new files

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -77,3 +77,16 @@
 // };
 
 // export default Footer;
+
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is my footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
