db9c1a73708b1c1635c6c145f3461c610d481cf2
dmanaglia <ddsmm.managlia@gmail.com>
2023-04-09 17:46:54 -0500

    added style choices and updated quiz quiestion pages to cards with style color

--- client/src/pages/CreateQuiz/CreateQuiz.js
+++ client/src/pages/CreateQuiz/CreateQuiz.js
@@ -223,6 +223,12 @@ const CreateQuiz = (props) => {
 							<label className='me-2'>Quiz Styling:</label>
 							<select name="quizStyling" defaultValue={quizValues.style} onChange={event => handleInfoChange(event)}>
 								<option value="default">Default</option>
+								<option value="brown">Brown</option>
+								<option value="green">Green</option>
+								<option value="red">Red</option>
+								<option value="yellow">Yellow</option>
+								<option value="orange">Orange</option>
+								<option value="pink">Pink</option>
 							</select>
 						</div>
 					</div>
